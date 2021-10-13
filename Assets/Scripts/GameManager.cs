4aaab30d675ec3feef815a10e84bdde862d954c8
Ryo <hana05ry@icloud.com>
2021-10-13 19:14:36 +0900

    フィールド関連の機能は作成完了．カードの入れ替え機能を作れば最低限動く

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,24 +6,78 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] Transform BattleFieldTransform;
     [SerializeField] Transform BenchFieldTransform;
+    [SerializeField] Transform SubBenchFieldTransfrom;
+    [SerializeField] GameObject Bench;
+    [SerializeField] GameObject SubBench;
     [SerializeField] GameObject cardPrefab;
+    bool bigBench;
+    Transform field;
+    int BenchObjCount;
+    int SubBenchObjCount;
 
     // Start is called before the first frame update
     void Start()
     {
-        CreateCard(BattleFieldTransform);
+        field = BattleFieldTransform;
+        GameObject card = Instantiate(cardPrefab, field, false) as GameObject;
+        bigBench = false;
+        field = BenchFieldTransform;
+    }
+
+    private void Update()
+    {
+        if (bigBench)
+        {
+            SubBench.SetActive(true);
+        }
+        else
+        {
+            SubBench.SetActive(false);
+        }
     }
 
     void CreateCard(Transform field)
     {
+        
+        //ベンチのカードの枚数でベンチを拡張するか決める（ムゲンダイナ）
+        BenchObjCount = Bench.transform.childCount;
+        SubBenchObjCount = SubBench.transform.childCount;
+
+
+        if (SubBenchObjCount >= 3)
+        {
+            return;
+        }
+        if (BenchObjCount >= 5 && bigBench && SubBenchObjCount < 3)
+        {
+            field = SubBenchFieldTransfrom;
+        }
+        else if(BenchObjCount >= 5 && !bigBench)
+        {
+            return;
+        }
+        else if(BenchObjCount < 5)
+        {
+            field = BenchFieldTransform;
+        }
         //カードの生成とデータの受け渡し
-        GameObject card = Instantiate(cardPrefab, field, false)　as GameObject;
+        GameObject card = Instantiate(cardPrefab, field, false) as GameObject;
     }
 
     public void CreateCardOnBench()
     {
-        CreateCard(BenchFieldTransform);
+        CreateCard(field);
     }
 
-    
+    public void ToggleBench8()
+    {
+        bigBench = !bigBench;
+        if (!bigBench)
+        {
+            foreach (Transform child in SubBench.transform)
+            {
+                Destroy(child.gameObject);
+            }
+        }
+    }
 }
